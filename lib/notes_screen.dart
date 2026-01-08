be2f8277ed37c63743eb6005916916df41381c7c
Prince <ghoshprince999@gmail.com>
2026-01-08 13:31:38 +0600

    update text to card

--- lib/notes_screen.dart
+++ lib/notes_screen.dart
@@ -41,12 +41,54 @@ class _NotesScreenState extends State<NotesScreen> {
     });
   }
 
+  // Read notes from supabase in app
+
+  final _notesStream = Supabase.instance.client
+      .from('notes')
+      .stream(primaryKey: ['id']);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: addNewNote,
         child: Icon(Icons.add),
+      ),
+      body: StreamBuilder<List<Map<String, dynamic>>>(
+        stream: _notesStream,
+        builder: (context, snapshot) {
+          //loading
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          //loaded
+          final notes = snapshot.data!;
+
+          return ListView.builder(
+            itemCount: notes.length,
+            itemBuilder: (context, index) {
+              //get individual note
+              final note = notes[index];
+
+              // get the column you want
+              final noteText = note['body'];
+
+              //return as UI
+              return Card(
+                margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                elevation: 3,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Text(noteText, style: const TextStyle(fontSize: 16)),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
