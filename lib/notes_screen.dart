f5b1a424c47f9b3f278ac41a81a0c67eb3307df7
Prince <ghoshprince999@gmail.com>
2026-01-08 12:51:48 +0600

    add note into the supabase

--- lib/notes_screen.dart
+++ lib/notes_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class NotesScreen extends StatefulWidget {
   const NotesScreen({super.key});
@@ -8,8 +9,45 @@ class NotesScreen extends StatefulWidget {
 }
 
 class _NotesScreenState extends State<NotesScreen> {
+  // CREATE - a note and save in supabase
+
+  final textController = TextEditingController();
+
+  void addNewNote() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: textController,
+          decoration: InputDecoration(hintText: 'Enter your note'),
+        ),
+        actions: [
+          //Save Button
+          TextButton(
+            onPressed: () {
+              saveNote();
+              Navigator.pop(context);
+            },
+            child: const Text('Save'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void saveNote() async {
+    await Supabase.instance.client.from('notes').insert({
+      'body': textController.text,
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: addNewNote,
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
